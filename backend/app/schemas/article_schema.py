0da39ab591419e61bd9447ed66e0f2f9407e87ff
Ey0s <eyosyasyosef1934@gmail.com>
2026-03-03 18:47:36 +0300

    add id, pubDate, score

--- backend/app/schemas/article_schema.py
+++ backend/app/schemas/article_schema.py
@@ -1,11 +1,20 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, HttpUrl
+from typing import List, Optional
+from datetime import datetime
 
-class Article(BaseModel):
+
+class Article(BaseModel):               
+    id: str                 
     title: str
-    link: str
+    link: HttpUrl
     summary: str
+    pubDate: Optional[datetime] = None
+    score: Optional[int] = 0
+
 
 class ArticleResponse(BaseModel):
     topic: str
+    page: int
+    size: int
+    total_results: int
     results: List[Article]
